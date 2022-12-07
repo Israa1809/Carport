5a5dcf4ce27a4db6bcfe96c934be0690d7e20962
malenec <90346924+malenec@users.noreply.github.com>
2022-12-07 13:49:55 +0100

    experiments with calcBeams method

--- src/main/java/dat/backend/model/entities/BillOfMaterials.java
+++ src/main/java/dat/backend/model/entities/BillOfMaterials.java
@@ -1,9 +1,16 @@
 package dat.backend.model.entities;
 
+import dat.backend.model.services.Calculator;
+
 import java.util.List;
 
 public class BillOfMaterials {
 
     private List<Material> MaterialList;
+
+    public static void main(String[] args) {
+        System.out.println(Calculator.calcBeams(620+160, 530+70));
+                                                // (1,etellerandet = 1 + 2 = 3 stolper) x (1 = 2) = 2 stolper // dvs i alt 4
+    }
 
 }
--- src/main/java/dat/backend/model/services/Calculator.java
+++ src/main/java/dat/backend/model/services/Calculator.java
@@ -2,11 +2,23 @@ package dat.backend.model.services;
 
 public class Calculator {
 
+    public static int calcBeams(int length, int width) {     //'beams' er 'stolper' på dansk
 
-        public static int calcBeams(int length, int width){     //'beams' er 'stolper' på dansk
-            int res = 4;
+//        int resLength = 2;
+//        int resWidth = 2;
+        int resTotal = 4 ;
 
-            return res;
+        float lengthDiv = (length-160)/310f; //vi har besluttet at længde og bredde måles i cm, og vi antager at hvis længden overstiger 310 cm skal der indsættes en ekstra stolpe
+        float widthDiv = (width-70)/530f; //vi har besluttet at længde og bredde måles i cm, og vi antager at hvis bredden overstiger 530 cm skal der indsættes en ekstra stolpe
+
+       if(lengthDiv > 1 && widthDiv > 1){
+           resTotal = ((int) lengthDiv + 1) * ((int) widthDiv + 1);
         }
 
+//        if (widthDiv > 1){
+//            resWidth = (int) widthDiv + 2;     // + 1 for at runde op fra det decimaltal man muligvis vil få
+//        }
+
+        return resTotal;
+    }
 }
