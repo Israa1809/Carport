6b0bb96bd68b819343da94f7d53272f941415d9e
Mark Pedersen <sutsko@gmail.com>
2022-12-20 19:22:49 +0100

    calcRoof Test

---
+++ src/test/java/dat/backend/model/services/CalculatorTest.java
@@ -0,0 +1,16 @@
+package dat.backend.model.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void calcRoof() {
+        int calc = Calculator.calcRoof(780,300,360);
+        int expected = 9;
+
+        assertEquals(calc, expected);
+    }
+}
